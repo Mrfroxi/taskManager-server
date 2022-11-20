65d4759de66876f9cc16f6145eb92ac31b17d86c
Mrfroxi <froxi340@mail.ru>
2022-11-21 01:13:51 +0300

    fix:fixed try-2

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -14,8 +14,9 @@ export class UsersController {
   async findUserById(@Param('id') id: string) {
     return this.userService.findUserById(id);
   }
+
   @Get()
   async findUsers() {
-    return this.userService.findUsers;
+    return this.userService.findUsers();
   }
 }
