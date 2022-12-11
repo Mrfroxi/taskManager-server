1d5bc3be9f7e0c7bae6cfe1163aff3321d46e666
Mrfroxi <froxi340@mail.ru>
2022-12-12 00:33:25 +0300

    feat :added cors setup

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:30000', 'https://www.google.com'],
+    origin: ['http://localhost:30000'],
     methods: ['POST', 'PUT', 'DELETE', 'GET'],
   });
 
