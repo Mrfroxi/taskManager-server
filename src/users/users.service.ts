817d518fff62e03cb95c24da8a5ddf3117f89bf9
Mrfroxi <74072824+Mrfroxi@users.noreply.github.com>
2023-02-07 19:09:54 +0300

    feat :changed setup for createUser

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -7,8 +7,11 @@ import { CreateUserDto } from './dto/create-user.dto';
 @Injectable()
 export class UsersService {
   constructor(
+  
     @InjectRepository(User)
+     
     private userRepository: Repository<User>,
+     
   ) {}
 
   async createUser(dto: CreateUserDto) {
